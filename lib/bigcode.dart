2707db16b26cd54d5cb6811538546f85bf42bcb6
enemie <enemiedrago@gmail.com>
2025-10-22 23:29:23 +0500

    changes

---
+++ lib/bigcode.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class idk extends StatefulWidget {
+  const idk({super.key});
+
+  @override
+  State<idk> createState() => _idkState();
+}
+
+class _idkState extends State<idk> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
